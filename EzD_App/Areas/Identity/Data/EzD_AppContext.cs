128946132dbdec16d014f8e14c739f328a3c6adf
Tamer <Masarwe.tamer@gmail.com>
2022-09-27 21:46:01 +0300

    initial commit

---
+++ EzD_App/Areas/Identity/Data/EzD_AppContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace EzD_App.Data
+{
+    public class EzD_AppContext : IdentityDbContext<IdentityUser>
+    {
+        public EzD_AppContext(DbContextOptions<EzD_AppContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ EzD_App/wwwroot/scripts/NavbarState.js
@@ -0,0 +1,50 @@
+﻿// This function will show or hide the 'Deliveries' option
+// depends if the user is a delivery-guy or not.
+function NavbarState() {
+
+    $.ajax({
+        dataType: "json",
+        url: "/api/User/NavbarState",
+        contentType: "application/json",
+        type: "POST",
+        success: function (data) {
+            if (data) {
+                document.getElementById("deliveries").style.display = "block";
+            }
+            else {
+                if (document.getElementById("deliveries"))
+                    document.getElementById("deliveries").style.display = "none";
+            }
+        },
+        error:
+            function (err) {
+                console.log("in error");
+                console.log(err);
+            }
+    });
+}
+
+// This function will get the email of the logged in user
+// and will change it in the navbar.
+function GetEmail() {
+
+    $.ajax({
+        dataType: "json",
+        url: "/api/User/GetEmailForNavbar",
+        contentType: "application/json",
+        type: "POST",
+        success: function (data) {
+            if (data == false) {
+                document.getElementById("profileName").innerHTML = "My Profile";
+            }
+            else if( data != null){
+                document.getElementById("profileName").innerHTML = data;
+            }
+        },
+        error:
+            function (err) {
+                console.log("in error");
+                console.log(err);
+            }
+    });
+}
